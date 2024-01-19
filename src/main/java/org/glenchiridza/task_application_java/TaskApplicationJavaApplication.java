50f8b4e991e85a31795e6d8965a9f0c4caf90d19
glenchiridza <glenchiridz@gmail.com>
2024-01-19 22:26:01 +0200

    java task app start

---
+++ src/main/java/org/glenchiridza/task_application_java/TaskApplicationJavaApplication.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.task_application_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplicationJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplicationJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/glenchiridza/task_application_java/TaskApplicationJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.glenchiridza.task_application_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskApplicationJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
