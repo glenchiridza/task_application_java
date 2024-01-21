505e2085723954befaa8930c8a31c169e422da73
glenchiridza <glenchiridz@gmail.com>
2024-01-21 21:45:47 +0200

    enums

---
+++ src/main/java/org/glenchiridza/task_application_java/enums/Priority.java
@@ -0,0 +1,5 @@
+package org.glenchiridza.task_application_java.enums;
+
+public enum Priority {
+    LOW,MID,HIGH
+}
--- src/main/java/org/glenchiridza/task_application_java/models/Task.java
+++ src/main/java/org/glenchiridza/task_application_java/models/Task.java
@@ -2,18 +2,37 @@ package org.glenchiridza.task_application_java.models;
 
 
 import jakarta.persistence.*;
+import lombok.NonNull;
+import org.glenchiridza.task_application_java.enums.Priority;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.time.LocalDateTime;
+
 
 @Entity
-@Table(name = "task",uniqueConstraints = {UniqueConstraint(name="uk_task_description",columnNames= "")})
+@Table(name = "task",uniqueConstraints = {@UniqueConstraint(name="uk_task_description",columnNames= {"description"})})
 public class Task {
 
     @Id
     @GeneratedValue(generator = "task_sequence",strategy = GenerationType.SEQUENCE)
     @SequenceGenerator(name = "task_sequence",sequenceName = "task_sequence",allocationSize = 1)
     private int id;
+
+    @Column(name = "description", unique = true)
+    private String description;
+
+    @Column(name="is_reminder_on",nullable = false)
+    private Boolean isReminderOn;
+
+    @Column(name = "is_task_pending",nullable = false)
+    private Boolean isTaskPending =true;
+
+    @Column(name = "created_date",nullable = false)
+    private LocalDateTime created_date;
+
+    @Column(name = "updated_date")
+    private LocalDateTime updatedDate;
+
+
+    private org.glenchiridza.task_application_java.enums.Priority Priority;
 
 }
