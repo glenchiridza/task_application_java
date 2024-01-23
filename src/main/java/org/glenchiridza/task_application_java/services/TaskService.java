5b8444b3c0e290340d8b4d4321784dd5ff3668cc
glenchiridza <glenchiridz@gmail.com>
2024-01-23 20:56:23 +0200

    di taskservice

--- src/main/java/org/glenchiridza/task_application_java/services/TaskService.java
+++ src/main/java/org/glenchiridza/task_application_java/services/TaskService.java
@@ -1,4 +1,16 @@
 package org.glenchiridza.task_application_java.services;
 
+import org.glenchiridza.task_application_java.repositories.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TaskService {
+
+    private TaskRepository taskRepository;
+
+    @Autowired
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
 }
